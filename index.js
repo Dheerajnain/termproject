b619d2a8ffbb86918e0ac38ca183c9646d4a889b
Dheeraj Nain <748416@student.tor.loc>
2019-08-07 17:20:27 -0400

    sd

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/*const http = require('http');
 const data = {
     firstName: 'Laurence',
     lastname: 'svekis'
@@ -12,4 +12,30 @@ const site = http.createServer(function(req, res) {
     res.write(JSON.stringify(data));
     res.end();
 });
-site.listen(3000);
+site.listen(3000);*/
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    console.log("I'm in the middle")
+    next();
+})
+app.get('/', function(req, res) {
+    res.send('<h1>Hello World</h1>')
+})
+app.post('/', function(req, res) {
+    console.log(req.body.username);
+    console.log(req.body.password);
+    res.send('post')
+})
+app.put('/', function(req, res) {
+    res.send('this is put')
+})
+app.delete('/', function(req, res) {
+    res.send('this is delete')
+})
+app.listen(3000)
